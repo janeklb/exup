6feea0da714ce3fe53d918d8c32942df72bb5ec0
Janek Lasocki-Biczysko <iamjanek@gmail.com>
2011-09-17 16:55:47 +0100

    Completed account editor

--- ftpaccounteditor.py
+++ ftpaccounteditor.py
@@ -1,91 +1,136 @@
 #!/usr/bin/python
 
-import sys, ConfigParser, m2secret, traceback
+import sys, ConfigParser, m2secret, traceback, exupconfig, ftplib, ftputil
 
-FTPOPTIONS = { 'host': False, 'user': False, 'pass': True, 'path': False }
+class FTPAccountEditor:
 
-# Edit an FTP config section
-def edit_ftp():
-	global hasedits
+	def __init__(self):
+		self.FTPOPTIONS 	= { 'host': False, 'user': False, 'pass': True, 'path': False }
+		self.hasedits 		= False
 
-	section = raw_input('Enter section to edit: ')
-	option = raw_input('Enter option to edit: ')
-	value = raw_input('Enter value for ' + option + ': ')
+		self.configfile 	= exupconfig.FTPCONFIGFILE
+		self.configfilefp 	= open(self.configfile)
 
-	if option in FTPOPTIONS and FTPOPTIONS[option] == True:
-		print 'serializing'
-		secret.encrypt(value, secretkey)
-		value = secret.serialize()
+		self.config 		= ConfigParser.RawConfigParser(allow_no_value=True)
+		self.config.readfp(self.configfilefp)
+		
+		print 'Loaded repositories:', self.config.sections()
 	
-	config.set(section, option, value)
-	hasedits = True
-	
-# Add an FTP config section
-def add_ftp():
-	global hasedits
-	
-	section = raw_input('Enter section name: ')
-	config.add_section(section)
-	hasedits = True
+	def is_option(self, option):
+		return option in self.FTPOPTIONS
+		
+	# Edit an FTP config section
+	def edit_ftp(self):
 
-	for option, encrypt in FTPOPTIONS.iteritems():
-		value = ''
-		if (encrypt):
-			secret.encrypt(raw_input('Enter ' + option + ': '), secretkey)
-			value = secret.serialize()
-		else:
+		repo = raw_input('Enter repository to edit: ')
+		if not self.config.has_section(repo):
+			print "This repository doesn't have any FTP info"
+			return
+		
+		option = raw_input('Enter option to edit: ')
+		if not self.is_option(option):
+			print "That's not a valid FTP option"
+			return
+		
+		value = raw_input('Enter value for ' + option + ': ')
+
+		if self.is_option(option) and self.FTPOPTIONS[option] == True and self.secretkey:
+			self.secret.encrypt(value, self.secretkey)
+			value = self.secret.serialize()
+	
+		self.config.set(repo, option, value)
+		self.hasedits = True
+	
+	# Add an FTP config section
+	def add_ftp(self):
+	
+		repo = raw_input('Enter repository name: ')
+		options = {}
+		rawpass = ''
+
+		for option, encrypt in self.FTPOPTIONS.iteritems():
 			value = raw_input('Enter ' + option + ': ')
-		print 'Setting', option, 'to', value
-		config.set(section, option, value)
+			if not value:
+				print "Empty options are invalid, please try again"
+				return
+			
+			if option == 'pass':
+				rawpass = value
 
-# Display FTP
-def display_ftp():
-	options = config.items(raw_input('Enter section name: '))
-	print options
+			if option == 'path' and value[0] != '/':
+				value = '/' + value
 
-def main():
-	hasedits 	= False
-	configfile 	= exupconfig.FTPCONFIGFILE
-	configfilefp 	= open(configfile)
+			if (encrypt and self.secretkey):
+				self.secret.encrypt(value, self.secretkey)
+				value = self.secret.serialize()
 
-	config 		= ConfigParser.RawConfigParser(allow_no_value=True)
-	config.readfp(configfilefp)
-
-	secret 		= m2secret.Secret()
-	secretkey 	= raw_input('Enter your encryption key for the FTP password: ')
-	print 'Loaded sections:', config.sections()
-
-	while (True):
+			options[option] = value
+		
 		try:
-			command = raw_input('Add (a), Edit (e), Display (d), Quit (q): ')
-			if command == 'a':
-				add_ftp()
-			elif command == 'e':
-				edit_ftp()
-			elif command == 'd':
-				display_ftp()
-			elif command == 'q':
+			print "Attempting connection: " + options['user'] + ":" + rawpass + "@" + options['host'] + options['path']
+			host = ftputil.FTPHost(options['host'], options['user'], rawpass)
+			if not host.path.isdir(options['path']):
+				raise Exception("Specified path is not a directory")
+		except Exception, err:
+			print err
+			print "Unable to connect using specified parameters -- options for " + repo + " will not be saved. Please try again."
+		else:
+			self.config.add_section(repo)
+			for option, value in options:
+				self.config.set(repo, option, value)
+			self.hasedits = True
+			print "Added settings for " + repo + ". Changes won't be saved until exit."
 
-				if hasedits:
-
-					save = raw_input('Would you like to save changes to ' + configfile + '? [Y/n]')
-
-					if save == 'Y' or save == 'y':
-						configfilefp.close()
-						with open(configfile, 'wb') as configfilefp:
-							config.write(configfilefp)
-							print configfile, 'saved'
-					
-				configfilefp.close()
-				print 'Bye!'
-				return 0
-			else:
-				print 'Unknown command'
-
-		except Exception:
-			traceback.print_exc()
+	# Display FTP
+	def display_ftp(self):
+		section = raw_input('Display repository: ')
+		if not self.config.has_section(section):
+			print "This repository doesn't have any FTP info"
+			return
+			
+		for o in self.config.items():
+			option = o[0]
+			value = o[1]
+			if self.is_option(option) and self.FTPOPTIONS[option] == True and self.secretkey:
+				self.secret.deserialize(value)
+				value = self.secret.decrypt(self.secretkey) + " (decrypted)"
+			print option + ": " + value
 	
-	return 0
+	def run(self):
+
+		self.secret 	= m2secret.Secret()
+		self.secretkey 	= raw_input('Enter your encryption key for the FTP password (or blank for no encryption): ')
+
+		while (True):
+			try:
+				command = raw_input('Add (a), Edit (e), Display (d), Quit (q): ')
+				if command == 'a':
+					self.add_ftp()
+				elif command == 'e':
+					self.edit_ftp()
+				elif command == 'd':
+					self.display_ftp()
+				elif command == 'q':
+
+					if self.hasedits:
+						if raw_input('Would you like to save changes to ' + configfile + '? [Y/N]').lowercase() == 'y':
+							self.configfilefp.close()
+
+							with open(self.configfile, 'wb') as self.configfilefp:
+								self.config.write(self.configfilefp)
+								print self.configfile, 'saved'
+					
+					self.configfilefp.close()
+					print 'Bye!'
+					return 0
+				else:
+					print 'Unknown command'
+			except Exception:
+				traceback.print_exc()
+	
+		return 0
+
 
 if __name__ == '__main__':
-    sys.exit(main())
+	ftpae = FTPAccountEditor()
+	sys.exit(ftpae.run())
